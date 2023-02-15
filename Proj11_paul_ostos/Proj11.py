de0147fdb208e2ccedf9053f609eabcef2cb3fb2
Paul Ostos <paulostos@Pauls-MacBook-Air.local>
2023-02-15 09:06:55 -0700

    Proj11

---
+++ Proj11_paul_ostos/Proj11.py
@@ -0,0 +1,26 @@
+#DO NOT MODIFY THE CODE IN THIS FILE
+#File: Proj11.py
+'''
+Among other things, this project requires you to 
+ Read a csv file containing a large amount of stock market data.
+ Extract specific values over a specific time period.
+ Process the data.
+ Display the processed data as colored lines on a line graph.
+'''
+
+from Proj11Runner import Runner
+import sys
+
+#Create and display a list of command-line arguments
+args = sys.argv[1].split(",")
+print()
+print('args = ',args)
+print()
+
+print("Output from student code begins here.")
+#Call the run method in the student's file passing
+#the args list as a parameter.
+Runner.run(args)
+
+
+
